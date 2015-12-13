9b09104eddd313c5dab5b97d5c477239fac388a0
Rex Zeng <rex.fgtsky@qq.com>
2015-12-13 15:03:23 +0800

    完成lfsr

--- lfsr/lfsr.cpp
+++ lfsr/lfsr.cpp
@@ -2,20 +2,20 @@
 #include <fstream>
 #include <vector>
 #include <cmath>
+#define MAX_LEN 31
 using namespace std; 
 
 int main() {   
-	int a[31] = {1, 1, 0, 0, 1}; 
-	for (int i = 5; i < 31; ++i)
+	int a[MAX_LEN] = {1, 1, 1, 0, 1}; 
+	for (int i = 5; i < MAX_LEN; ++i)
 		a[i] = (a[i - 2] + a[i - 5]) % 2;
 	cout << "密钥如下：" << endl;
-	for (int i = 0; i<31; ++i)
-		cout << a[i] << " ";
+	for (int i = 0; i < MAX_LEN; ++i)
+		cout << a[i];
 	cout << endl;
 	int i = 0, j = 0, key;
 	cout << "请选择操作方式" << endl << "1:加密clear.txt中的内容" << endl << "2:解密cipher.txt中的内容" << endl;
 	cin >> key;
-	vector<int> s, ss;
 	ifstream in;
 	ofstream out;
 	if (key == 1) {
@@ -31,12 +31,13 @@ int main() {
 		return 1;
 	} 
 	char c;
+	vector<int> s;
 	while ((c = in.get()) != EOF) {
 		int sum = 0;
 		for (j = 0; j < 8; ++j)
-			sum += pow(2, 7 - j) * a[(i + j) % 31];
+			sum += pow(2, 7 - j) * a[(i + j) % MAX_LEN];
 		if (i + j > 32)
-			i = (i + j - 1) % 31 + 1;
+			i = (i + j - 1) % MAX_LEN + 1;
 		else
 			i += 8;
 		s.push_back((int(c)) ^ sum);
